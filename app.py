31cf8949f815869caced9074ff526d4e4687656f
Brittney Kistner <bkistner01@gmail.com>
2015-07-12 09:34:11 -0700

    Part 2 of tutorial

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 import os
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
-print(os.environ['APP_SETTINGS'])
+# Detecting the correct environment variables for a specific environment
+# print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
